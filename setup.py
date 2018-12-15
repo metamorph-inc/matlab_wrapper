d0b39e952c4c748f989f8b29afb3422e1231a67a
Kevin Smyth <kevin.m.smyth@gmail.com>
2018-12-15 16:23:46 -0600

    Version 0.5

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 setup(
     name="matlab_wrapper",
-    version="0.4",
+    version="0.5",
     packages=find_packages(),
     # FIXME do we need openmdao>=1.5.0,<2.0.0
-    install_requires=["openmdao>=1.5.0", "smop>=0.23", "six>=1.10.0"]
+    install_requires=["openmdao==1.*", "smop>=0.23", "six>=1.10.0"]
 )
