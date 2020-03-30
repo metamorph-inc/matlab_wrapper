2a5f2613a472fe0ebf78251964f8364718b593f0
Jonathon Williams <jwilliams@metamorphsoftware.com>
2020-03-30 17:52:00 -0500

    v0.9; correctly handle case where no MATLAB is installed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="matlab_wrapper",
-    version="0.8",
+    version="0.9",
     packages=find_packages(),
     # FIXME do we need openmdao>=1.5.0,<2.0.0
     install_requires=["openmdao==1.*", "smop>=0.23", "six>=1.10.0"]
