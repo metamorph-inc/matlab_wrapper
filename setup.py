1ae8745654111c7d60fdb7730d4331d6f4fcceae
Jonathon Williams <jwilliams@metamorphsoftware.com>
2021-12-15 12:39:14 -0600

    Bump to version 0.13 (version needed to be updated in setup.py)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="matlab_wrapper",
-    version="0.10",
+    version="0.13",
     packages=find_packages(),
     install_requires=["openmdao>=1.5,<2", "smop>=0.23", "six>=1.10.0"],
     project_urls={
