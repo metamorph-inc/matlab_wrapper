1448e38dcbbbb197335cfe7a238783379245746b
Kevin Smyth <kevin.m.smyth@gmail.com>
2023-03-03 11:39:59 -0600

    v0.16

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="matlab_wrapper",
-    version="0.15",
+    version="0.16",
     packages=find_packages(),
     install_requires=["openmdao>=1.5,<2", "smop>=0.23", "six>=1.10.0"],
     project_urls={
